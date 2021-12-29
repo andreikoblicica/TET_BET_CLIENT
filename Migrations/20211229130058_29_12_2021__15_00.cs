4aecd6ff080e88f0249a9e0883286a47d05f3485
StoicaTudor <56082968+StoicaTudor@users.noreply.github.com>
2021-12-29 15:02:02 +0200

    odd value to bet not userbet

---
+++ Migrations/20211229130058_29_12_2021__15_00.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TET_BET.Migrations
+{
+    public partial class _29_12_2021__15_00 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "oddValue",
+                table: "DBUserBet");
+
+            migrationBuilder.AddColumn<float>(
+                name: "oddValue",
+                table: "DBBet",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "oddValue",
+                table: "DBBet");
+
+            migrationBuilder.AddColumn<float>(
+                name: "oddValue",
+                table: "DBUserBet",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f);
+        }
+    }
+}
